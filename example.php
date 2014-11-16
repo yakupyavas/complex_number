518905e2275448d558bfa1e9e56afb17a551318f
Yakup Yavaş <ykp481@hotmail.com>
2014-11-16 11:11:02 +0200

    Create example.php

---
+++ example.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'complex.php';
+
+	#Examples
+	$complex = new Complex(); //instance
+	$complex->set_comp_num(3,4); //define number
+	echo"Number: ".$complex->comp_numb."<br>";
+	echo"Real: ".$complex->real."<br>";
+	echo"Imaginer: ".$complex->imaginer."<br>";
+	echo "Modulus: ".$complex->modulus()."<br>";
+	echo "Polar Form: ".$complex->polar_form()."<br>";
+	echo "Conjugate Number: ".$complex->conj_number()."<br>";
+	echo "Conjugate Form: ".$complex->conj_form()."<br>";
+?>
